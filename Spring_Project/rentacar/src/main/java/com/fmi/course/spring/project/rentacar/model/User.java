68fcb30bb3de48aa3a11c4180cd3183bd471059b
ekalambova <ekalambova@vmware.com>
2020-02-09 18:53:03 +0200

    init car

--- Spring_Project/rentacar/src/main/java/com/fmi/course/spring/project/rentacar/model/User.java
+++ Spring_Project/rentacar/src/main/java/com/fmi/course/spring/project/rentacar/model/User.java
@@ -51,7 +51,6 @@ public class User implements UserDetails {
     @PastOrPresent
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime modified = LocalDateTime.now();
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Arrays.asList(roles.split("\\s*,\\s*")).stream()
